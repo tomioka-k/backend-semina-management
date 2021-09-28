e4cd78c4584b83ff725e93d9d86c86991926884a
tomioka-k <tomioka-k@tajima.co.jp>
2021-09-29 06:47:08 +0900

    seminar/modesl.py edit

--- seminar/models.py
+++ seminar/models.py
@@ -20,33 +20,40 @@ class Seminar(models.Model):
     )
 
     # Basic
-    name = models.CharField(max_length=255)
-    method = models.CharField(max_length=10, choices=method_choices)
-    affiliation = models.CharField(max_length=5, choices=affiliation_choices)
-    staff = models.CharField(max_length=50)
-    place = models.CharField(max_length=255)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    start_application_date = models.DateField(null=True)
-    end_application_date = models.DateField(null=True)
-    number_of_attendees = models.IntegerField(default=0)
-    capacity = models.IntegerField(null=True)
+    name = models.CharField(verbose_name="セミナー名", max_length=255)
+    method = models.CharField(
+        verbose_name="開催方法", max_length=10, choices=method_choices)
+    affiliation = models.CharField(
+        "所属", max_length=5, choices=affiliation_choices)
+    staff = models.CharField(verbose_name="担当者", max_length=50)
+    place = models.CharField("場所", max_length=255)
+    start_date = models.DateField(verbose_name="開催日")
+    end_date = models.DateField(verbose_name="終了日")
+    start_application_date = models.DateField(verbose_name="申込開始日", null=True)
+    end_application_date = models.DateField(verbose_name="申込締切日", null=True)
+    number_of_attendees = models.IntegerField(verbose_name="出席者", default=0)
+    capacity = models.IntegerField(verbose_name="定員", null=True)
 
-    application_page = models.URLField(max_length=200, blank=True)
-    application_list = models.URLField(max_length=200, blank=True)
-    autience_page = models.URLField(max_length=200, blank=True)
-    event_page = models.URLField(max_length=200, blank=True)
-    is_reserved = models.BooleanField(default=False)
+    application_page = models.URLField(
+        verbose_name="申込ページ", max_length=200, blank=True)
+    application_list = models.URLField(
+        verbose_name="申込者一覧", max_length=200, blank=True)
+    audience_page = models.URLField(
+        verbose_name="視聴ページ", max_length=200, blank=True)
+    event_page = models.URLField(
+        verbose_name="イベントページ", max_length=200, blank=True)
+    is_reserved = models.BooleanField(verbose_name="ZOOM予約", default=False)
 
     # Mail
-    invitation_mail = models.DateField(null=True)
-    remaind_mail = models.DateField(null=True)
-    viewing_guide_mail = models.DateField(null=True)
-    thank_you_mail = models.DateField(null=True)
+    invitation_mail = models.DateField(verbose_name="招待メール配信日", null=True)
+    remaind_mail = models.DateField(verbose_name="リマインドメール配信日", null=True)
+    viewing_guide_mail = models.DateField(verbose_name="視聴案内メール配信日", null=True)
+    thank_you_mail = models.DateField(verbose_name="御礼メール配信日", null=True)
 
-    creater = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    creater = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(verbose_name="作成日", auto_now=True)
+    updated_at = models.DateTimeField(verbose_name="更新日", auto_now_add=True)
 
     def __str__(self):
         return self.name
